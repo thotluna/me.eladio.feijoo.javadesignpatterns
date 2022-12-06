64f303e5ecb20f56b569081dc6ecddffe17666c6
thotluna <eladi.feijoo@gmail.com>
2022-12-05 23:03:08 -0400

    add singleton pattern

--- src/main/java/me/eladiofeijoo/javadesingnpatterns/singleton/ConnectionSingleton.java
+++ src/main/java/me/eladiofeijoo/javadesingnpatterns/singleton/ConnectionSingleton.java
@@ -1,2 +1,25 @@
-package me.eladiofeijoo.javadesingnpatterns.singleton;public class ConnectionSingleton {
+package me.eladiofeijoo.javadesingnpatterns.singleton;
+
+public class ConnectionSingleton {
+    private static ConnectionSingleton instance;
+    private int counter = 0;
+    private ConnectionSingleton(){
+
+    }
+
+    public static ConnectionSingleton getInstance(){
+        if(instance == null){
+            instance = new ConnectionSingleton();
+
+        }
+        return instance;
+    }
+
+    public void incrementCounter(){
+        counter++;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
 }
